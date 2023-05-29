fb8c5f31b6f03e8a6ae511a9b3ade36ca2404b62
Hank Lee <hankhplee@gmail.com>
2023-05-29 12:22:38 -0400

    Add MutationObserver to the script to monitor DOM changes dynamically generated images are now captured and blurred by the script.

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const image_size_threshold = 50 // image height threashold for image blurring
 function initialUpdates() {
   console.log("Applying custom style to existing img tags.");
   // const elements = document.getElementsByTagName("img");
+  blur_img(document);
+  /*
   const elements = document.querySelectorAll("image,img");
   [...elements].forEach(e => {
     if(e.clientWidth>image_size_threshold){
@@ -16,8 +18,26 @@ function initialUpdates() {
   [...twitter_elements].forEach(e => {
     e.style = "filter: grayscale(100%) blur(5px)";
   });
+  */
 }
 
+function blur_img(document_query){
+  // basic
+  const elements = document_query.querySelectorAll("image,img");
+  [...elements].forEach(e => {
+    if(e.clientWidth>image_size_threshold){
+      e.style = "filter: grayscale(100%) blur(5px); opacity: 1;";
+    }
+  });
+
+  // twitter custom style
+  // const twitter_elements = document_query.querySelectorAll("div[style^='background-image:']");
+  // [...twitter_elements].forEach(e => {
+  //   if(e.clientWidth>image_size_threshold){
+  //     e.style = "filter: grayscale(100%) blur(5px)";
+  //   }
+  // });
+}
 
 // default event listener
 document.querySelectorAll("image,img").forEach(function(elem) {
@@ -57,4 +77,40 @@ document.querySelectorAll("div[style^='background-image:']").forEach(function(el
 // });
 
 // document.getElementsByTagName('video')[0].removeAttribute('autoplay');
-initialUpdates()
+
+
+// when changes happen in the webpage, update images
+// document.body.addEventListener(
+//     'DOMNodeInserted',
+//     function(){
+//       initialUpdates()
+//     }
+// );
+
+
+var mutationObserver = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) {
+      // console.log(mutation);
+      if(mutation.type != "characterData"){
+        blur_img(mutation.target);
+      }
+      // var mut_elements = mutation.target.querySelectorAll("image,img");
+      // [...mut_elements].forEach(e => {
+      //   if(e.clientWidth>image_size_threshold){
+      //     e.style = "filter: grayscale(100%) blur(5px)";
+      //   }
+      // });
+  });
+});
+
+// Starts listening for changes in the root HTML element of the page.
+mutationObserver.observe(document.documentElement, {
+  attributes: true,
+  characterData: true,
+  childList: true,
+  subtree: true,
+  attributeOldValue: true,
+  characterDataOldValue: true
+});
+// Takes all changes which haven’t been fired so far.
+var changes = mutationObserver.takeRecords();
