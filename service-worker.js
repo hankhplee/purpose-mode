3cde629462dfdccd9a79e375553338e283b134a8
Philipp Winter <phw@brave.com>
2023-06-05 16:59:28 -0500

    Refresh open tab when Purpose Mode is disabled.
    It's not the ideal solution but it will do in the meanwhile.

--- service-worker.js
+++ service-worker.js
@@ -29,6 +29,14 @@ function updateState(changes, area) {
     chrome.scripting.unregisterContentScripts({ids: [extName]}) // check if the content script exist
       .then(console.log("Unregistered content script."));
     chrome.action.setIcon({path: {"128": "icons/purpose-mode-off.png"}});
+    // Refresh the page, with Purpose Mode disabled.  Note that this only works
+    // for the tab that's currently open.  Other tabs will still have Purpose
+    // Mode lingering in them.  To work around this, we need the content script
+    // to periodically poll the service worker to learn if Purpose Mode is still
+    // enabled.
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.reload(tabs[0].id);
+    });
   } else {
     chrome.scripting.registerContentScripts([{
         id: extName,
