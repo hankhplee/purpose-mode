e4b22a949cbc3f0efd9c660e64470188fcfb2e85
Hank Lee <hankhplee@gmail.com>
2023-07-13 02:03:41 -0400

    Updated ESM study popup layout

--- popup.tsx
+++ popup.tsx
@@ -544,9 +544,9 @@ function IndexPopup() {
         padding: 16,
         width: "300px"
       }}>
-    <div id="hero">
-      <div>
-        <div className="has-text-right">
+    <div id="hero" className="block">
+      <div className="columns is-mobile">
+        <div className="column has-text-right">
           <div id="dropdown_setting" className="dropdown is-right">
             <div className="dropdown-trigger">
               <span>
@@ -583,12 +583,9 @@ function IndexPopup() {
             </div>
           </div>
         </div>
-        <div className="has-text-centered">
-          <h2 className="title is-6"> {extName}</h2>
-        </div>
       </div>
     </div>
-    <nav className="level is-mobile">
+    <nav className="level is-mobile block">
       <div className="level-item has-text-centered">
         <div>
           <p className="heading">Today Answered</p>
