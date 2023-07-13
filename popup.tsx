026112609b47214d52dddd3dff11155d5dcd4f84
Hank Lee <hankhplee@gmail.com>
2023-07-13 01:52:45 -0400

    Added ESM study interface - added test notification (require permission setting in package.json)

--- popup.tsx
+++ popup.tsx
@@ -543,7 +543,11 @@ function IndexPopup() {
         </div>
       </div>
     </div>
+<<<<<<< HEAD
     <nav className="level is-mobile block">
+=======
+    <nav className="level is-mobile">
+>>>>>>> 9cf21d4 (Added ESM study interface)
       <div className="level-item has-text-centered">
         <div>
           <p className="heading">Today Answered</p>
@@ -562,6 +566,7 @@ function IndexPopup() {
         <button className="button is-info is-small" id="questionnaire">Questionnaire</button>
       </div>
     </nav>
+<<<<<<< HEAD
 
       <div className="block">  
         <ExpandableMenu
@@ -570,6 +575,8 @@ function IndexPopup() {
             Switches={AutoPlaySwitch}
             />
       </div>
+=======
+>>>>>>> 9cf21d4 (Added ESM study interface)
 
       <div className="box hero is-primary">
         <ToggleSwitch
